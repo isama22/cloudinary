3751bc2c1b74ea1d3d74e8dcfa243a225a64f493
isama22 <emarti17@artic.edu>
2021-02-17 17:54:38 -0600

    somehwo fixed 404 server issue

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const app = express()
 app.use(express.json({limit: '50mb' }))
 app.use(express.urlencoded({limit: '50mb', extended: true}))
 
-
 app.post('/api/upload', (req, res)=> {
     try {
         const fileStr = req.body.data
