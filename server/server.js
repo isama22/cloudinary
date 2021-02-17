2c870e09b498bd99e2da43a9243a23c17e3a0226
isama22 <emarti17@artic.edu>
2021-02-17 16:53:49 -0600

    added server file

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+
+app.use(express.json({limit: '50mb' }))
+app.use(express.urlencoded({limit: '50mb', extended: true}))
+
+
+app.post('/api/upload', (req, res)=> {
+    try {
+        const fileStr = req.body.data
+        console.log(fileStr)
+    } catch (error){
+        console.log(error)
+    }
+})
+
+
+const port = process.env.port || 3001
+
+app.listen(3001, () => {
+    console.log(`listening on port ${port}`)
+})
